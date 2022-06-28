97de33226fc3bd6a226f7959690b48cd6d5f0075
Emily Schell <lauraemilyschell@gmail.com>
2022-06-28 16:29:13 -0400

    adds components and skeleton CSS for Card and UpdateBoardForm, some callbacks/state in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import UpdateBoardForm from './Components/UpdateBoardForm';
+import axios from 'axios';
+
+const URL = 'https://ma5en-inspo-board-be.herokuapp.com/';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [boardData, setBoardData] = useState([]);
+    const [selected, setSelected] = useState(boardData[0]);
+
+    const getBoards = () => {
+        axios
+            .get(`${URL}/boards`)
+            .then((response) => {
+                const newData = response.data.map((board) => {
+                    return {
+                        id: board.board_id,
+                        title: board.title,
+                        owner: board.owner,
+                    };
+                });
+                setBoardData(newData);
+            })
+            .catch((err) => {
+                alert(err);
+            });
+    };
+
+    useEffect(() => getBoards(), []);
+
+    const updateBoard = (newBoardInfo) => {
+        const newBoardData = boardData.map((board) => {
+            if (board.id === selected.id) {
+                return newBoardInfo;
+            } else {
+                return board;
+            }
+        });
+        setBoardData(newBoardData);
+    };
+
+    return (
+        <div className='App'>
+            <UpdateBoardForm updateBoardCallback={updateBoard} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/UpdateBoardForm.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Forms.css';
+
+const UpdateBoardForm = ({ updateBoardCallback }) => {
+    const blankFormData = { title: '', owner: '' };
+
+    const [formData, setFormData] = useState(blankFormData);
+    const [hide, setHide] = useState(true);
+
+    const updateFormData = (e) => {
+        const newFormData = {
+            ...formData,
+            [e.target.name]: e.target.value,
+        };
+        setFormData(newFormData);
+    };
+
+    const updateBoard = (e) => {
+        e.preventDefault();
+        updateBoardCallback(formData);
+        setFormData(blankFormData);
+    };
+
+    const shown = hide ? 'hidden' : 'shown';
+
+    return (
+        <div>
+            <button onClick={() => setHide(!hide)}>
+                {(hide ? 'Show' : 'Hide') + ' Update Board Form'}
+            </button>
+            <div className={shown}>
+                <h2>Update Board</h2>
+                <form onSubmit={updateBoard}>
+                    <label htmlFor='title'>Title</label>
+                    <input
+                        onChange={updateFormData}
+                        name='title'
+                        id='title'
+                        type='text'
+                    />
+                    <label htmlFor='owner'>Owner</label>
+                    <input
+                        onChange={updateFormData}
+                        name='owner'
+                        id='owner'
+                        type='owner'
+                    />
+                    <button type='submit'>Update Board</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+UpdateBoardForm.propTypes = {
+    updateBoardCallback: PropTypes.func.isRequired,
+};
+
+export default UpdateBoardForm;
