b6d42a8e174b6abc2b61e3021a455ceac479af7f
Jande Royalty <janderoyalty@gmail.com>
2022-06-29 11:32:16 -0500

    Added Axios methods to post, get, and delete one board

---
+++ src/components/Board.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+// import Card from "./Card";
+
+const Board = () => {
+  // const allCards = props.cards.map((card) => {
+  //   return (
+  //     <Card
+  //       key={card.id}
+  //       id={card.id}
+  //       message={card.message}
+  //       like_count={card.like_count}
+  //       board_id={card.board_id}
+  //       board={card.board}
+  //     ></Card>
+  //   );
+  // });
+  // return <div>{allCards}</div>;
+};
+
+// Board.propTypes = {
+//   cards: PropTypes.arrayOf(PropTypes.object).isRequired,
+// };
+
+export default Board;
