af6c2a8e396b14dea8f48b0c59b043404a0274cb
Jande Royalty <janderoyalty@gmail.com>
2022-06-30 11:47:23 -0500

    remerge to get new board function back at the top

--- src/Components/BoardList.js
+++ src/Components/BoardList.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 // need to import board
 
 // NEED TO CREATE CSS FILE
 
-const BoardList = ({ boards, selectBoard }) => {
-    const displayBoard = (boards) => {
-        return boards.map((board, index) => {
-            return (
-                <li key={index} onClick={() => selectBoard(board.id)}>
-                    {board.title}
-                </li>
-            );
-        });
-    };
+const BoardList = ({ boards, selectBoard, onDeleteCallback }) => {
+  const displayBoard = (boards) => {
+    return boards.map((board, index) => {
+      return (
+        <li key={index} onClick={() => selectBoard(board.id)}>
+          {board.title}
+        </li>
+      );
+    });
+  };
 
-    return (
-        <section>
-            <ul>{displayBoard(boards)}</ul>
-        </section>
-    );
+  return (
+    <section>
+      <ul>{displayBoard(boards)}</ul>
+      <button onClick={() => onDeleteCallback(boards.id)}>🗑</button>
+    </section>
+  );
 };
 
 BoardList.propTypes = {
-    boards: PropTypes.array.isRequired,
+  boards: PropTypes.array.isRequired,
 };
 
 export default BoardList;
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,102 +1,103 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Card from './Card';
-import { URL } from '../App';
-import NewCardForm from './NewCardForm';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "./Card";
+import { URL } from "../App";
+import NewCardForm from "./NewCardForm";
 
 const Board = ({ boardId }) => {
-    const [cardData, setCardData] = useState([]);
+  const [cardData, setCardData] = useState([]);
 
-    const getCards = () => {
-        axios
-            .get(`${URL}/boards/${boardId}/cards`)
-            .then((response) => {
-                const newData = response.data.map((card) => {
-                    return {
-                        id: card.card_id,
-                        message: card.message,
-                        likeCount: card.like_count,
-                    };
-                });
-                setCardData(newData);
-            })
-            .catch((err) => {
-                alert(err);
-            });
-    };
+  const getCards = () => {
+    axios
+      .get(`${URL}/boards/${boardId}/cards`)
+      .then((response) => {
+        const newData = response.data.map((card) => {
+          return {
+            id: card.card_id,
+            message: card.message,
+            likeCount: card.like_count,
+          };
+        });
+        setCardData(newData);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
 
-    useEffect(() => getCards(), [cardData]);
+  useEffect(() => getCards(), [cardData]);
 
-    const createCard = () => {};
+  const createCard = () => {};
 
-    const updateCard = async (id, message) => {
-        try {
-            await axios.patch(`${URL}/cards/${id}`, { message });
-            const newCardData = cardData.map((card) => {
-                if (card.id === id) {
-                    return {
-                        ...card,
-                        [message]: message,
-                    };
-                } else {
-                    return card;
-                }
-            });
-            setCardData(newCardData);
-        } catch (err) {
-            alert(err);
+  const updateCard = async (id, message) => {
+    try {
+      await axios.patch(`${URL}/cards/${id}`, { message });
+      const newCardData = cardData.map((card) => {
+        if (card.id === id) {
+          return {
+            ...card,
+            [message]: message,
+          };
+        } else {
+          return card;
         }
-    };
+      });
+      setCardData(newCardData);
+    } catch (err) {
+      alert(err);
+    }
+  };
 
-    const deleteCard = async (id) => {
-        try {
-            await axios.delete(`${URL}/cards/${id}`);
-            const newCardData = cardData.filter((card) => card.id !== id);
-            setCardData(newCardData);
-        } catch (err) {
-            alert(err);
+  const deleteCard = async (id) => {
+    try {
+      await axios.delete(`${URL}/cards/${id}`);
+      const newCardData = cardData.filter((card) => card.id !== id);
+      setCardData(newCardData);
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const updateLikes = async (id) => {
+    try {
+      await axios.patch(`${URL}/cards/${id}/like`);
+      const newCardData = cardData.map((card) => {
+        if (card.id === id) {
+          return {
+            ...card,
+            likeCount: card.likeCount + 1,
+          };
+        } else {
+          return card;
         }
-    };
+      });
+      setCardData(newCardData);
+    } catch (err) {
+      alert(err);
+    }
+  };
 
-    const updateLikes = async (id) => {
-        try {
-            await axios.patch(`${URL}/cards/${id}/like`);
-            const newCardData = cardData.map((card) => {
-                if (card.id === id) {
-                    return {
-                        ...card,
-                        likeCount: card.likeCount + 1,
-                    };
-                } else {
-                    return card;
-                }
-            });
-            setCardData(newCardData);
-        } catch (err) {
-            alert(err);
-        }
-    };
-
-    return (
-        <div className='board'>
-            <div className='cards'>
-                {cardData.map((card, index) => {
-                    return (
-                        <Card
-                            key={index}
-                            id={card.id}
-                            message={card.message}
-                            likeCount={card.likeCount}
-                            updateLikes={updateLikes}
-                            deleteCard={deleteCard}
-                            updateCard={updateCard}
-                        />
-                    );
-                })}
-            </div>
-            <NewCardForm createCardFunction={createCard} />
-        </div>
-    );
+  return (
+    <div className="board">
+      <NewCardForm createCardFunction={createCard} />
+      <div className="cards">
+        <h1>{Board.title}</h1>
+        {cardData.map((card, index) => {
+          return (
+            <Card
+              key={index}
+              id={card.id}
+              message={card.message}
+              likeCount={card.likeCount}
+              updateLikes={updateLikes}
+              deleteCard={deleteCard}
+              updateCard={updateCard}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Board;
