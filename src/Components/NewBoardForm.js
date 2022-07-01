078296426b66e62f8d1ed21fc65f9b72cb115a39
Jande Royalty <janderoyalty@gmail.com>
2022-06-30 22:46:46 -0500

    added PropTypes to New Board and Card Forms

--- src/Components/NewBoardForm.js
+++ src/Components/NewBoardForm.js
@@ -1,63 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 const NewBoardForm = ({ onAddBoardCallback }) => {
-    const [boardData, setBoardData] = useState({
-        title: '',
-        owner: '',
-    });
-    const [hide, setHide] = useState(true);
+  const [boardData, setBoardData] = useState({
+    title: "",
+    owner: "",
+  });
+  const [hide, setHide] = useState(true);
 
-    const submitBoardData = (event) => {
-        event.preventDefault();
-        onAddBoardCallback(boardData);
-        setBoardData({ title: '', owner: '' });
-    };
+  const submitBoardData = (event) => {
+    event.preventDefault();
+    onAddBoardCallback(boardData);
+    setBoardData({ title: "", owner: "" });
+  };
 
-    const handleBoardChange = (event) => {
-        setBoardData({ ...boardData, [event.target.name]: event.target.value });
-    };
+  const handleBoardChange = (event) => {
+    setBoardData({ ...boardData, [event.target.name]: event.target.value });
+  };
 
-    const shown = hide ? 'hidden' : 'shown';
+  const shown = hide ? "hidden" : "shown";
 
-    return (
-        <div>
-            <button onClick={() => setHide(!hide)}>
-                {hide ? 'Show Add Board Form' : 'Hide Add Board Form'}
-            </button>
-            <div className={shown}>
-                <form onSubmit={submitBoardData} className='new-board__form'>
-                    <section>
-                        <h2>Add New Board</h2>
-                        <div className='new-board__fields'>
-                            <div>
-                                <label htmlFor='title'>Title</label>
-                                <input
-                                    name='title'
-                                    id='title'
-                                    value={boardData.title}
-                                    onChange={handleBoardChange}
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor='owner'>Owner</label>
-                                <input
-                                    name='owner'
-                                    id='owner'
-                                    value={boardData.owner}
-                                    onChange={handleBoardChange}
-                                />
-                            </div>
-                            <button
-                                className='button new-board__submit'
-                                type='submit'>
-                                Add Board
-                            </button>
-                        </div>
-                    </section>
-                </form>
+  return (
+    <div>
+      <button onClick={() => setHide(!hide)}>
+        {hide ? "Show Add Board Form" : "Hide Add Board Form"}
+      </button>
+      <div className={shown}>
+        <form onSubmit={submitBoardData} className="new-board__form">
+          <section>
+            <h2>Add New Board</h2>
+            <div className="new-board__fields">
+              <div>
+                <label htmlFor="title">Title</label>
+                <input
+                  name="title"
+                  id="title"
+                  value={boardData.title}
+                  onChange={handleBoardChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="owner">Owner</label>
+                <input
+                  name="owner"
+                  id="owner"
+                  value={boardData.owner}
+                  onChange={handleBoardChange}
+                />
+              </div>
+              <button className="button new-board__submit" type="submit">
+                Add Board
+              </button>
             </div>
-        </div>
-    );
+          </section>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+NewBoardForm.propTypes = {
+  onAddBoardCallback: PropTypes.func.isRequired,
 };
 
 export default NewBoardForm;
