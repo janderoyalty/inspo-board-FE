322847f4e75da2f63cb4a02b747b98ede6d05698
Emily Schell <lauraemilyschell@gmail.com>
2022-07-18 16:06:14 -0400

    fixes bug in update and add board that was giving alert but still processing invalid data

--- src/App.js
+++ src/App.js
@@ -39,20 +39,16 @@ const App = () => {
 
     useEffect(() => getBoards(), [boardData]);
 
-    const validateBoardData = (newBoardInfo) => {
+    const updateBoard = (newBoardInfo) => {
         if (!newBoardInfo['title']) {
             return alert('New board must have a title!');
         }
         if (!newBoardInfo['owner']) {
             return alert('New board must have an owner!');
-        } else return true;
-    };
-
-    const updateBoard = (newBoardInfo) => {
-        validateBoardData(newBoardInfo) &&
-            axios
-                .put(`${URL}/boards/${selected.id}`, newBoardInfo)
-                .catch((err) => alert(err));
+        }
+        axios
+            .put(`${URL}/boards/${selected.id}`, newBoardInfo)
+            .catch((err) => alert(err));
         const newBoardData = boardData.map((board) => {
             if (board.id === selected.id) {
                 return { ...newBoardInfo, id: selected.id };
@@ -65,16 +61,21 @@ const App = () => {
     };
 
     const addBoard = ({ title, owner }) => {
-        validateBoardData({ title, owner }) &&
-            axios
-                .post(`${URL}/boards`, {
-                    title: title,
-                    owner: owner,
-                })
-                .then((response) => {})
-                .catch((error) => {
-                    console.log(error);
-                });
+        if (!title) {
+            return alert('New board must have a title!');
+        }
+        if (!owner) {
+            return alert('New board must have an owner!');
+        }
+        axios
+            .post(`${URL}/boards`, {
+                title: title,
+                owner: owner,
+            })
+            .then((response) => {})
+            .catch((error) => {
+                console.log(error);
+            });
     };
 
     const deleteBoard = (boardId) => {
