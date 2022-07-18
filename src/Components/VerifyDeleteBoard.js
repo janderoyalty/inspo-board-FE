d0d76dc3c846464c3f37ba076403e9153c881ace
Emily Schell <lauraemilyschell@gmail.com>
2022-07-18 15:44:02 -0400

    adds confirmation check on delete board with new component plus css

---
+++ src/Components/VerifyDeleteBoard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './VerifyDeleteBoard.css';
+
+const VerifyDeleteBoard = ({ onDeleteCallback, onCancelCallback, id }) => {
+    return (
+        <div className='verify-delete-board'>
+            <p>Are you sure you want to delete this board?</p>
+            <button
+                className='verify-delete-board__button'
+                onClick={() => onDeleteCallback(id)}>
+                {' '}
+                Delete Board
+            </button>
+            <button
+                className='verify-delete-board__button'
+                onClick={() => onCancelCallback()}>
+                {' '}
+                Cancel
+            </button>
+        </div>
+    );
+};
+
+VerifyDeleteBoard.propTypes = {
+    onDeleteCallback: PropTypes.func.isRequired,
+    onCancelCallback: PropTypes.func.isRequired,
+    id: PropTypes.number.isRequired,
+};
+
+export default VerifyDeleteBoard;
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,187 +1,204 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Card from "./Card";
-import { URL } from "../App";
-import NewCardForm from "./NewCardForm";
-import UpdateBoardForm from "./UpdateBoardForm";
-import SortMenu from "./SortMenu";
-import "./Board.css";
-import PropTypes from "prop-types";
-import { BiSort } from "react-icons/bi";
-import { TiDeleteOutline } from "react-icons/ti";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from './Card';
+import { URL } from '../App';
+import NewCardForm from './NewCardForm';
+import UpdateBoardForm from './UpdateBoardForm';
+import SortMenu from './SortMenu';
+import './Board.css';
+import PropTypes from 'prop-types';
+import { BiSort } from 'react-icons/bi';
+import { TiDeleteOutline } from 'react-icons/ti';
+import VerifyDeleteBoard from './VerifyDeleteBoard';
 
 const Board = ({ board, onDeleteCallback, updateBoardCallback }) => {
-  const [cardData, setCardData] = useState([]);
-  const [sortBy, setSortBy] = useState("id");
-  const [orderBy, setOrderBy] = useState("desc");
-  const [hide, setHide] = useState(true);
+    const [cardData, setCardData] = useState([]);
+    const [sortBy, setSortBy] = useState('id');
+    const [orderBy, setOrderBy] = useState('desc');
+    const [hide, setHide] = useState(true);
+    const [deleteAttempt, setDeleteAttempt] = useState(false);
 
-  const shown = hide ? "hidden" : "shown";
+    const shown = hide ? 'hidden' : 'shown';
 
-  const getCards = () => {
-    axios
-      .get(`${URL}/boards/${board.id}/cards`)
-      .then((response) => {
-        const newData = response.data.map((card) => {
-          return {
-            id: card.card_id,
-            message: card.message,
-            likeCount: card.like_count,
-          };
-        });
-        setCardData(newData);
-      })
-      .catch((err) => {
-        alert(err);
-      });
-  };
+    const getCards = () => {
+        axios
+            .get(`${URL}/boards/${board.id}/cards`)
+            .then((response) => {
+                const newData = response.data.map((card) => {
+                    return {
+                        id: card.card_id,
+                        message: card.message,
+                        likeCount: card.like_count,
+                    };
+                });
+                setCardData(newData);
+            })
+            .catch((err) => {
+                alert(err);
+            });
+    };
 
-  useEffect(() => getCards(), [cardData]);
+    useEffect(() => getCards(), [cardData]);
 
-  const sortedCards = cardData.sort((a, b) => {
-    let order = orderBy === "asc" ? 1 : -1;
-    return a[sortBy] < b[sortBy] ? -1 * order : 1 * order;
-  });
+    const sortedCards = cardData.sort((a, b) => {
+        let order = orderBy === 'asc' ? 1 : -1;
+        return a[sortBy] < b[sortBy] ? -1 * order : 1 * order;
+    });
 
-  const validateCardData = (newCardInfo) => {
-    if (!newCardInfo["message"]) {
-      return alert("New card must have a message!");
-    } else if (newCardInfo["message"].length > 40) {
-      return alert(
-        `Messages cannot be more than 40 characters, yours is ${newCardInfo["message"].length} long!`
-      );
-    } else return true;
-  };
-
-  const addNewCard = (newCard) => {
-    validateCardData(newCard) &&
-      axios
-        .post(`${URL}/boards/${board.id}/cards`, newCard)
-        .then((response) => {
-          console.log("a new card has been posted");
-          const cards = [...cardData, response.data];
-          setCardData(cards);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-  };
-
-  const updateCard = (id, message) => {
-    validateCardData({ message }) &&
-      axios
-        .patch(`${URL}/cards/${id}`, { message })
-        .then(() => {
-          const newCardData = cardData.map((card) => {
-            if (card.id === id) {
-              return {
-                ...card,
-                [message]: message,
-              };
-            } else {
-              return card;
-            }
-          });
-          setCardData(newCardData);
-        })
-        .catch((err) => {
-          alert(err);
-        });
-  };
-
-  const deleteCard = async (id) => {
-    try {
-      await axios.delete(`${URL}/cards/${id}`);
-      const newCardData = cardData.filter((card) => card.id !== id);
-      setCardData(newCardData);
-    } catch (err) {
-      alert(err);
-    }
-  };
-
-  const updateLikes = async (id) => {
-    try {
-      await axios.patch(`${URL}/cards/${id}/like`);
-      const newCardData = cardData.map((card) => {
-        if (card.id === id) {
-          return {
-            ...card,
-            likeCount: card.likeCount + 1,
-          };
-        } else {
-          return card;
-        }
-      });
-      setCardData(newCardData);
-    } catch (err) {
-      alert(err);
-    }
-  };
-
-  return (
-    <div>
-      <div className="board--title ">
-        <h1 className="insetshadow">{board.title}</h1>
-        <h2 className="insetshadow">by {board.owner}</h2>
-      </div>
-      <div className="board--nav">
-        <div>
-          <BiSort className="icons" size={30} onClick={() => setHide(!hide)}>
-            {hide ? "Show" : "Hide"}
-          </BiSort>
-          <section className={shown}>
-            <div className="sort-menu--container">
-              <SortMenu
-                sortBy={sortBy}
-                onSortByChange={(sortOption) => {
-                  setSortBy(sortOption);
-                }}
-                orderBy={orderBy}
-                onOrderByChange={(orderOption) => {
-                  setOrderBy(orderOption);
-                }}
-              />
-            </div>
-          </section>
-        </div>
-        <UpdateBoardForm updateBoardCallback={updateBoardCallback} />
-        <TiDeleteOutline
-          className="icons"
-          size={30}
-          onClick={() => onDeleteCallback(board.id)}
-        />
-        <NewCardForm boardId={board.id} onAddCardCallback={addNewCard} />
-      </div>
-      <div className="board--cards">
-        <div className="cards">
-          {sortedCards.map((card, index) => {
-            return (
-              <Card
-                key={index}
-                id={card.id}
-                message={card.message}
-                likeCount={card.likeCount}
-                updateLikes={updateLikes}
-                deleteCard={deleteCard}
-                updateCard={updateCard}
-              />
+    const validateCardData = (newCardInfo) => {
+        if (!newCardInfo['message']) {
+            return alert('New card must have a message!');
+        } else if (newCardInfo['message'].length > 40) {
+            return alert(
+                `Messages cannot be more than 40 characters, yours is ${newCardInfo['message'].length} long!`
             );
-          })}
+        } else return true;
+    };
+
+    const addNewCard = (newCard) => {
+        validateCardData(newCard) &&
+            axios
+                .post(`${URL}/boards/${board.id}/cards`, newCard)
+                .then((response) => {
+                    console.log('a new card has been posted');
+                    const cards = [...cardData, response.data];
+                    setCardData(cards);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+    };
+
+    const updateCard = (id, message) => {
+        validateCardData({ message }) &&
+            axios
+                .patch(`${URL}/cards/${id}`, { message })
+                .then(() => {
+                    const newCardData = cardData.map((card) => {
+                        if (card.id === id) {
+                            return {
+                                ...card,
+                                [message]: message,
+                            };
+                        } else {
+                            return card;
+                        }
+                    });
+                    setCardData(newCardData);
+                })
+                .catch((err) => {
+                    alert(err);
+                });
+    };
+
+    const deleteCard = async (id) => {
+        try {
+            await axios.delete(`${URL}/cards/${id}`);
+            const newCardData = cardData.filter((card) => card.id !== id);
+            setCardData(newCardData);
+        } catch (err) {
+            alert(err);
+        }
+    };
+
+    const updateLikes = async (id) => {
+        try {
+            await axios.patch(`${URL}/cards/${id}/like`);
+            const newCardData = cardData.map((card) => {
+                if (card.id === id) {
+                    return {
+                        ...card,
+                        likeCount: card.likeCount + 1,
+                    };
+                } else {
+                    return card;
+                }
+            });
+            setCardData(newCardData);
+        } catch (err) {
+            alert(err);
+        }
+    };
+
+    return (
+        <div>
+            <div className='board--title '>
+                <h1 className='insetshadow'>{board.title}</h1>
+                <h2 className='insetshadow'>by {board.owner}</h2>
+            </div>
+            <div className='board--nav'>
+                <div>
+                    <BiSort
+                        className='icons'
+                        size={30}
+                        onClick={() => setHide(!hide)}>
+                        {hide ? 'Show' : 'Hide'}
+                    </BiSort>
+                    <section className={shown}>
+                        <div className='sort-menu--container'>
+                            <SortMenu
+                                sortBy={sortBy}
+                                onSortByChange={(sortOption) => {
+                                    setSortBy(sortOption);
+                                }}
+                                orderBy={orderBy}
+                                onOrderByChange={(orderOption) => {
+                                    setOrderBy(orderOption);
+                                }}
+                            />
+                        </div>
+                    </section>
+                </div>
+                <UpdateBoardForm updateBoardCallback={updateBoardCallback} />
+                <TiDeleteOutline
+                    className='icons'
+                    size={30}
+                    onClick={() => setDeleteAttempt(true)}
+                />
+                {deleteAttempt && (
+                    <VerifyDeleteBoard
+                        onDeleteCallback={onDeleteCallback}
+                        onCancelCallback={() => {
+                            setDeleteAttempt(false);
+                        }}
+                        id={board.id}
+                    />
+                )}
+                <NewCardForm
+                    boardId={board.id}
+                    onAddCardCallback={addNewCard}
+                />
+            </div>
+            <div className='board--cards'>
+                <div className='cards'>
+                    {sortedCards.map((card, index) => {
+                        return (
+                            <Card
+                                key={index}
+                                id={card.id}
+                                message={card.message}
+                                likeCount={card.likeCount}
+                                updateLikes={updateLikes}
+                                deleteCard={deleteCard}
+                                updateCard={updateCard}
+                            />
+                        );
+                    })}
+                </div>
+                {/* <NewCardForm boardId={board.id} onAddCardCallback={addNewCard} /> */}
+            </div>
         </div>
-        {/* <NewCardForm boardId={board.id} onAddCardCallback={addNewCard} /> */}
-      </div>
-    </div>
-  );
+    );
 };
 
 Board.propTypes = {
-  board: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    owner: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-  }),
-  onDeleteCallback: PropTypes.func.isRequired,
-  updateBoardCallback: PropTypes.func.isRequired,
+    board: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        owner: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+    }),
+    onDeleteCallback: PropTypes.func.isRequired,
+    updateBoardCallback: PropTypes.func.isRequired,
 };
 
 export default Board;
